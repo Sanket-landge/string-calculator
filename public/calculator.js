23ee8a9be760b33d9807878397e192e29d14b741
Sanket Landge <sanket.landge@polyglotconsultants.com>
2024-10-08 22:50:35 +0530

    refactor the code to let the add method handle new lines between numbers

--- public/calculator.js
+++ public/calculator.js
@@ -3,7 +3,7 @@ function add(numbers) {
 
     if (numbers === "") return 0;
 
-    const delimiter = ",";
+    const delimiter = /,|\n/;
     
     const nums = numbers.split(delimiter).map((num) => parseInt(num));
     return nums.reduce((sum, num) => sum + (isNaN(num) ? 0 : num), 0);
